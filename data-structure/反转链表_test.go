783ec0ac54a3b6f750bc82a742ee144cfe9285f0
saltyfishWangc <741627532@qq.com>
2023-06-04 22:03:36 +0800

    追加联系

--- data-structure/反转链表_test.go
+++ data-structure/反转链表_test.go
@@ -1,1 +1,10 @@
 package data_structure
+
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		cur.Next, pre, cur = pre, cur, cur.Next
+	}
+	return cur
+}
