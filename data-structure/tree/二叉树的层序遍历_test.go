d653f710e9a2e1de0a8d8749cdb1d4d111d81b89
saltyfishWangc <741627532@qq.com>
2023-06-06 23:17:19 +0800

    搜索二叉树的练习

--- data-structure/tree/二叉树的层序遍历_test.go
+++ data-structure/tree/二叉树的层序遍历_test.go
@@ -1,1 +1,50 @@
 package tree
+
+import "testing"
+
+func TestLevelOrder(t *testing.T) {
+	root := &TreeNode{Val: 3}
+	left := &TreeNode{Val: 9}
+	right := &TreeNode{Val: 20}
+	rightLeft := &TreeNode{Val: 15}
+	rightRight := &TreeNode{Val: 7}
+
+	root.Left = left
+	root.Right = right
+
+	right.Left = rightLeft
+	right.Right = rightRight
+
+	t.Log(levelOrder(root))
+}
+
+// levelOrder 给你一个二叉树的根节点root，返回其节点值的层序遍历。(即逐层地，从左到右访问所有节点)
+func levelOrder(root *TreeNode) [][]int {
+
+	var res [][]int
+	// 定义节点数组p，用来存储每一层的节点
+	var p []*TreeNode
+	// 初始化时只有根结点
+	p = append(p, root)
+	for len(p) > 0 {
+		// 用来存储每一层节点的值
+		var eachLevel []int
+
+		// 用来存储每一层节点的子节点，最后赋值给p，达到每一层一层遍历的效果
+		var tmp []*TreeNode
+		for _, node := range p {
+			eachLevel = append(eachLevel, node.Val)
+			if node.Left != nil {
+				tmp = append(tmp, node.Left)
+			}
+			if node.Right != nil {
+				tmp = append(tmp, node.Right)
+			}
+			//tmp = append(tmp, node.Left, node.Right)
+		}
+		res = append(res, eachLevel)
+
+		p = tmp
+	}
+	return res
+}
--- data-structure/tree/将有序数组转换为二叉搜索树_test.go
+++ data-structure/tree/将有序数组转换为二叉搜索树_test.go
@@ -1,1 +1,36 @@
 package tree
+
+import "testing"
+
+func TestSortedArrayToBST(t *testing.T) {
+	nums := []int{-10, -3, 0, 5, 9}
+	sortedArrayToBST(nums)
+}
+
+// sortedArrayToBST 将有序数组转换为二叉搜索树
+// 给定一个整数数组，其中元素已经按升序排列，将其转换为一棵 高度平衡 二叉搜索树
+// 高度平衡二叉树是一棵满足【每个节点的左右两个子树的高度差的绝对值不超过1】的二叉树
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	if len(nums) == 1 {
+		return &TreeNode{Val: nums[0]}
+	}
+
+	return sortedArrayToBSTIt(nums, 0, len(nums)-1)
+}
+
+// sortedArrayToBSTIt 因为数组是有序的，按照二叉树的特点来，左子树一定是小于根结点的，右子树一定是大于根结点的
+// 所以有序数组中的中间结点就是树的根结点，然后用中间结点的左边作为树的左子树，右边作为树的右子树
+func sortedArrayToBSTIt(nums []int, start, end int) *TreeNode {
+	if start > end {
+		return nil
+	}
+	mid := (end-start)/2 + start
+	root := &TreeNode{Val: nums[mid]}
+	root.Left = sortedArrayToBSTIt(nums, start, mid-1)
+	root.Right = sortedArrayToBSTIt(nums, mid+1, end)
+	return root
+}
